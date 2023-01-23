0ac02f488d591229d6a854851e072b4bd33f9db0
Ayse Nur Cifci <ayse.nur.cifci23@gmail.com>
2023-01-23 23:30:11 +0300

    Odeev dosyası hazırlandı

---
+++ kodgripckcam/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace ayseNur // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi = 0;
+
+            while (sayi < 10)
+            {
+                sayi++;
+                if (sayi == 7) continue;
+                Console.WriteLine(sayi);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
